9c8e85ae601cc2c94f5491c3f17b7981171af9c6
ccamp113 <ccamp113@students.kennesaw.edu>
2016-03-14 16:27:00 -0400

    Update Token.java

--- Token.java
+++ Token.java
@@ -1,8 +1,7 @@
 /* File: Token.java
  Name: Cameron Campbell
  CS4308-02
- Valid Token Types. enum type enables for Tokentype
- to be the following set of predefined constants */
+ */
 
 import java.io.FileNotFoundException;
 import java.util.List;
