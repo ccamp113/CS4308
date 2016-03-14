bd2b457b363a720bde268a7ad54a3552ad219053
ccamp113 <ccamp113@students.kennesaw.edu>
2016-03-14 15:21:30 -0400

    Update ArithmeticOperator.java

--- ArithmeticOperator.java
+++ ArithmeticOperator.java
@@ -1,8 +1,7 @@
-/* File: Token.java
+/* File: ArithmeticOperator.java
  Name: Cameron Campbell
  CS4308-02
- Valid Token Types. enum type enables for Tokentype
- to be the following set of predefined constants */
+ */
 public enum ArithmeticOperator {
     ADD_OP, MUL_OP, SUB_OP, DIV_OP
 }
